861c9f3f31fd7ddc81998db89ac3fe359f52f614
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-20 19:45:30 -0400

    removed unused import

--- src/MovePaddleMouseListener.java
+++ src/MovePaddleMouseListener.java
@@ -1,5 +1,4 @@
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
 
 public class MovePaddleMouseListener extends MouseMotionAdapter {
