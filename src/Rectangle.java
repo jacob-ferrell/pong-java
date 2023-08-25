3dcd4927c92b1762c0316700a305bd507f5f1b06
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-25 19:05:35 -0400

    removed unused method for getting corner positions

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -17,17 +17,6 @@ public class Rectangle {
         g2.setColor(color);
         g2.fill(new Rectangle2D.Double(this.position.x, this.position.y, width, height));
     }
-    public HashMap<String, List<Position>> getCorners() {
-        HashMap<String, List<Position>> corners = new HashMap<>();
-        List<Position> leftCorners = new ArrayList<>(), rightCorners = new ArrayList<>();
-        leftCorners.add(this.position);
-        leftCorners.add(new Position(this.position.x, this.getBottomY()));
-        corners.put(Constants.LEFT, leftCorners);
-        rightCorners.add(new Position(this.getRightX(), this.position.y));
-        rightCorners.add(new Position(this.getRightX(), this.getBottomY()));
-        corners.put(Constants.RIGHT, rightCorners);
-        return corners;
-    }
     public double getRightX() {
         return position.x + width;
     }
