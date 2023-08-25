cc154a8e08d3d6b30e4cc21aa70f125740dcd28b
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-25 19:07:10 -0400

    removed unused includes method and added method for getting random double within the instances range

--- src/Range.java
+++ src/Range.java
@@ -1,11 +1,10 @@
+import java.util.Random;
+
 public class Range {
     public double min, max;
     public Range(double min, double max) {
         this.min = min;
         this.max = max;
-    }
-    public boolean includes(double n) {
-        return n >= this.min && n <= this.max;
     }
     public Range getSharedRange(Range otherRange) {
         double sharedMin = Math.max(this.min, otherRange.min);
@@ -19,6 +18,10 @@ public class Range {
     }
     public double getAverage() {
         return min + ((max - min) / 2);
+    }
+    public double getRandom() {
+        Random random = new Random();
+        return random.nextDouble(this.min, this.max);
     }
 
     @Override
