fd3f87f67c6190cc995b970ecd17fbf75f2871fe
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-25 15:33:02 -0400

    removed unused properties

--- src/Angle.java
+++ src/Angle.java
@@ -2,11 +2,8 @@
 public class Angle {
     double x1, x2, y1, y2;
     double deltaX, deltaY;
-    public double vx, vy;
     public double inRadians;
     public double inDegrees;
-    public double againstVertical;
-    public double ofReflection;
     public Angle(Position p1, Position p2) {
         x1 = p1.x;
         y1 = p1.y;
@@ -21,14 +18,10 @@ public class Angle {
         this.inRadians = Math.toRadians(degrees);
         this.deltaX = Math.cos(inRadians);
         this.deltaY = Math.sin(inRadians);
-        this.againstVertical = Math.PI / 2 - inRadians;
-        this.ofReflection = Math.PI - inRadians;
     }
     private void init() {
         inRadians = Math.atan2(deltaY, deltaX);
         inDegrees = Math.toDegrees(inRadians);
-        againstVertical = Math.PI / 2 - inRadians;
-        ofReflection = Math.PI - inRadians;
     }
     public VelocityPair getVelocities() {
         return new VelocityPair(Math.cos(inRadians), Math.sin(inRadians));
