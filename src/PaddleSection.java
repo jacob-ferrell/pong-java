c34d857b04d8e3fd1987a5d6cf53eefd2770decd
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-25 19:04:09 -0400

    Removed unnecessary classes

--- src/PaddleSection.java
+++
@@ -1,9 +0,0 @@
-public class PaddleSection {
-    public Range range;
-    public Angle angleOfReflection;
-    public PaddleSection(Range range, int index) {
-        this.range = range;
-        double[] reflectionAngles = Constants.PADDLE_SECTION_REFLECTION_ANGLES;
-        angleOfReflection = new Angle(reflectionAngles[index]);
-    }
-}
