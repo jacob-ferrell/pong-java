0e4ced0d163a4f7eb5a530f6bb76fc94b76d8eb3
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-25 19:05:12 -0400

    removed no longer needed constructor

--- src/VelocityPair.java
+++ src/VelocityPair.java
@@ -1,9 +1,5 @@
 public class VelocityPair {
     public double vx, vy;
-//    public VelocityPair(double radians, double speed) {
-//        this.vx = Math.cos(radians) * speed;
-//        this.vy = Math.sin(radians) * speed;
-//    }
     public VelocityPair(double vx, double vy) {
         this.vx = vx;
         this.vy = vy;
