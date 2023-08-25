d7eb3d663baeca4b8fd0919f44049abfe3206388
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-08-25 19:02:47 -0400

    Gave each paddle their own methods for getting an end serve position

--- src/LeftPaddle.java
+++ src/LeftPaddle.java
@@ -1,15 +1,25 @@
+import java.util.Random;
+
 public class LeftPaddle extends Paddle {
     public LeftPaddle() {
         super(
                 Constants.PADDLE_WIDTH,
                 Constants.LEFT_PADDLE_START_POSITION,
                 new Range(Constants.HZ_CENTER * 1.25, Constants.SCREEN_WIDTH - 5),
-                Constants.PADDLE_HORIZONTAL_ANGLES[0]
+                Constants.PADDLE_HORIZONTAL_ANGLES[0],
+                Constants.LEFT_X_SERVE_START_POINT
         );
+    }
+    @Override
+    public Position getRandomServeEndPosition() {
+        var side = new Position(Constants.SCREEN_WIDTH, new Range(Constants.TOOLBAR_HEIGHT + 1, Constants.SCREEN_HEIGHT - 1));
+        Position[] walls = {getRandomTopServePosition(), side, getRandomBottomServePosition()};
+        return walls[new Random().nextInt(3)];
     }
     @Override
     public double getNormalizedDistance() {
         double centerPoint = this.height / 2;
         return this.getMedianImpactPoint() - centerPoint;
     }
+
 }
--- src/RightPaddle.java
+++ src/RightPaddle.java
@@ -1,15 +1,24 @@
+import java.util.Random;
+
 public class RightPaddle extends Paddle {
     public RightPaddle() {
         super(
                 Constants.SCREEN_WIDTH - Constants.PADDLE_WIDTH,
                 Constants.RIGHT_PADDLE_START_POSITION,
                 new Range(5, Constants.HZ_CENTER * 0.75),
-                Constants.PADDLE_HORIZONTAL_ANGLES[1]
+                Constants.PADDLE_HORIZONTAL_ANGLES[1],
+                Constants.RIGHT_X_SERVE_START_POINT
         );
     }
     @Override
     public double getNormalizedDistance() {
         double centerPoint = this.height / 2;
         return -(this.getMedianImpactPoint() - centerPoint);
+    }
+    @Override
+    public Position getRandomServeEndPosition() {
+        var side = new Position(0, new Range(Constants.TOOLBAR_HEIGHT + 1, Constants.SCREEN_HEIGHT - 1));
+        Position[] walls = {getRandomTopServePosition(), side, getRandomBottomServePosition()};
+        return walls[new Random().nextInt(3)];
     }
 }
